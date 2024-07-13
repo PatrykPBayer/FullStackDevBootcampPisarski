2bab318c3780bd4734ebf7589b6b7f8974516b5e
PatrykPBayer <patryk.pisarski@bayer.com>
2024-07-13 11:43:20 +0200

    Operatory

--- myscript.js
+++ myscript.js
@@ -33,3 +33,34 @@ let cars = ["Saab", "Volvo", "BMW"]
 //Date
 const date = new Date("2024-08-31")     //date - w formacie yyyy-mm-dd
 console.log(date)
+
+//Porównywanie danych
+
+//Equal
+console.log(length==weight)  //sam wynik
+console.log("Porownanie len vs weight: " + (length==weight))    //wynik do zwrócenia w raz z opisem jako string
+
+//Equal value and equal type
+console.log(length === weight)
+
+//Not equal
+console.log(100 != 150)     //można tez wpisac same liczby
+
+//Not equal and type
+console.log(100 !== "100")
+
+//Greater
+console.log(100>99)
+
+//Less
+console.log(100< 110)
+
+//greater or eq
+console.log(100 >= 100)
+
+//Less or eq
+console.log(100 <= 100)
+
+//Ternary operator - zwraca konketne wyrażenie w zależności od wyniku
+let result = 100 > 98 ? "Tak" : "Nie"
+console.log(result)
