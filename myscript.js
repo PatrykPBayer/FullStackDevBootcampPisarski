b0e1de00649f168921fe74ba905e9eefa03d8f7f
PatrykPBayer <patryk.pisarski@bayer.com>
2024-07-13 11:31:00 +0200

    data types js

---
+++ myscript.js
@@ -0,0 +1,35 @@
+//alert do wywołania na stronie
+// alert("Hello world")
+
+//Numeric - typ danych
+//len - tworzenie zmiennej
+//let length = 180
+//alert(length)   //wywołanie zmiennej w alercie
+
+console.log(length)  //wywołanie zmiennej w konsoli
+
+let weight = 85.5
+//alert(weight)
+console.log(weight)
+
+
+//Strings - typ danych
+let color = "Yellow"
+console.log(color)
+
+//Boolean - typ zmiennej
+let x = true
+console.log(x)
+let y = false
+console.log(y)
+
+//Object - definiowanie obiektu - typ zmiennej
+const person = {firstName: "Patryk", lastName: "Pisarski"}      //const jako zmienna niezmienna
+console.log(person)
+
+//Array - typ zmiennej
+let cars = ["Saab", "Volvo", "BMW"]
+
+//Date
+const date = new Date("2024-08-31")     //date - w formacie yyyy-mm-dd
+console.log(date)
