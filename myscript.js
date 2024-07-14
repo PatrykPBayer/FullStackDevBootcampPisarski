2bf9acf92328c5cc684925871607bf41e2f35831
PatrykPBayer <patryk.pisarski@bayer.com>
2024-07-14 12:03:57 +0200

    Patterns Observer

--- myscript.js
+++ myscript.js
@@ -1,56 +1,50 @@
 //Wzorce projektowe - gotowe rozwiązania na istniejące problemy
 
-//Factory - ułątwia tworzenie nowych obiektów określonego (stworzonego typu)
+//Obserwator/Observer
 
-//Klasyczna klasa
-/*
-class Product{
-    display(){
-        console.log("Product")
+function Click(){
+    this.handlers = []
+}
+
+Click.prototype = {
+
+    subscribe: function (fn){
+        this.handlers.push(fn)
+    },
+
+    unsubscribe: function(fn){
+        this.handlers = this.handlers.filter(
+            function (item){
+                if (item !== fn){
+                    return item
+                }
+            }
+        )
+    },
+
+    fire: function (o, thisObj){
+        var scope = thisObj || window
+        this.handlers.forEach(function(item){
+            item.call(scope, o)
+        })
     }
 }
 
-//Klasa product factory do częstego tworzenia
-class ProductFactory{
-    createProduct(){
-        return new Product()
+function run(){
+    var clickHandler = function(item){
+        console.log("Fired: " + item)
     }
+
+    var click = new Click()
+
+    click.subscribe(clickHandler)
+    click.fire("Event #1")
+    click.unsubscribe(clickHandler)
+    click.fire("Event #2")
+    click.subscribe(clickHandler)
+    click.fire("Event #3")
 }
 
-const factory = new ProductFactory()
-
-const product = factory.createProduct();    //średnik
-const product2 = factory.createProduct();
-product.display()
-product2.display()
-*/
-
-//z parametrami
-
-class Product{
-
-    constructor(width, height, weight){
-        this.width = width
-        this.height = height
-        this.weight = weight
-    }
-
-    display(){
-        console.log("Product: " + this.width + " " + this.height + " " + this.weight)
-    }
+window.onclick = function(){
+    run()
 }
-
-//Klasa product factory do częstego tworzenia
-class ProductFactory{
-    createProduct(width, height, weight){
-        return new Product(width, height, weight)
-    }
-}
-
-const factory = new ProductFactory()
-
-const product = factory.createProduct(20, 100, 90);    //średnik nieobowiązkowy
-const product2 = factory.createProduct(30, 60, 90);
-product.display()
-product2.display()
-
