95ccf3d32c1e1d27f9d0ba29694f053930f50677
PatrykPBayer <patryk.pisarski@bayer.com>
2024-07-14 10:32:12 +0200

    Promise Await Async

--- myscript.js
+++ myscript.js
@@ -26,15 +26,124 @@ window.onload = function(){
 }
 */
 //Promise, wywołanie dwóch różnych funkcji w zależności od warunku
-
+/*
 const promise = new Promise(( resolve, reject) => {
-    if(1==1){
+    if(1==2){
         resolve("ALL OK")
     }else{
         reject("NOT OK")
     }
 })
-
-promise.then(result => {
-    console.log(result)
+*/
+//druga opcja z timout jako symulowaniem pobierania danych
+/*
+function doSomething() {
+return new Promise(( resolve, reject) => {
+    setTimeout(() => {
+        //resolve("Data downloaded succesfully")    
+        reject("Error while data downloading")
+    }, 3000)
 })
+}
+
+doSomething()
+    .then(result => {
+        console.log("Success")
+        console.log("Success")
+        console.log("Success")
+    })
+    .catch(error => {
+        console.log(error)
+    })
+*/
+//promise.then(result => {
+//    console.log(result)
+//})
+
+//ASYNC, AWAIT
+
+//Standardowe podejście
+/*
+function doThings(){
+    return Promise.resolve("ok")
+}
+
+doThings()
+    .then(res => {
+        console.log(res)
+    })
+
+//ASYNC
+
+async function doThingsAsync(){
+    return "Ok"
+}
+
+doThingsAsync()
+    .then(res => {
+        console.log(res)
+    })
+
+//Async z błędem
+
+async function doThingsAsync(){
+    return Promise.reject("not ok")
+}
+
+doThingsAsync()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+*/
+
+//Cwiczenie na odpytywanie endpointów i czekanie aż się dane załadują
+
+function loadUserData(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("A")
+        }, 1000)
+    })
+}
+
+//Funkcja z błędęm, do obsłuzenia
+function loadBooks(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            //resolve("Book")
+            reject("Books not downloaded")
+        }, 1000)
+    })
+}
+
+function loadPets(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Pet")
+        }, 1000)
+    })
+}
+
+async function render(){
+    const user = await loadUserData()
+    //const books = await loadBooks()
+    let books
+    loadBooks()
+    .then(res => {
+        books = res
+    })
+    .catch(err => {
+        //books = err
+        console.log(err)
+        books = " "
+    })
+    const pets = await loadPets()
+    console.log(user, books, pets)
+}
+
+window.onload = function(){
+    render()
+}
