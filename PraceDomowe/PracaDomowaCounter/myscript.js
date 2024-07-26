7fa01aab01b3c0e9dad4108cb535c470b4984d8d
PatrykPBayer <patryk.pisarski@bayer.com>
2024-07-26 22:51:44 +0200

    Praca domowa na pozniej

---
+++ PraceDomowe/PracaDomowaCounter/myscript.js
@@ -0,0 +1,16 @@
+//Write a function that will count down how much time is left until a given date.
+//In the function, use "setInterval" to refresh the result every second, and then present it on the HTML page using DOM "getElementById"
+
+
+window.onload = function(){
+    document.getElementById("CounterStart").onclick = function(){
+        let targetDate = Date(document.getElementById("inputDate").value)
+        let currentDate = new Date()
+        //const action = document.getElementById("inputAction").value
+
+        
+
+    }
+}
+console.log(targetDate)
+console.log(currentDate)
